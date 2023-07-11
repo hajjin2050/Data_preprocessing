05fc851a318ede6436e9bf308aed3c572022877d
jjin2050 <76682924+hajjin2050@users.noreply.github.com>
2023-07-11 16:55:31 +0900

    Update ExG_ by_RGB.py

--- ExG_ by_RGB.py
+++ ExG_ by_RGB.py
@@ -17,7 +17,6 @@ def calculate_exg(image):
 def save_image(image, output_path):
     cv2.imwrite(output_path, image)
 
-from osgeo import gdal, osr
 
 def remove_soil_color(image):
     # Convert the image to HSV color space
@@ -35,26 +34,26 @@ def remove_soil_color(image):
 
     return image
 
-def save_image_with_geoinfo(input_path, output_image, output_path):
-    # Open the input image and get its geotransform and projection
-    input_ds = gdal.Open(input_path)
-    geotransform = input_ds.GetGeoTransform()
-    projection = input_ds.GetProjection()
-    input_ds = None  # Close the dataset
+# def save_image_with_geoinfo(input_path, output_image, output_path):
+#     # Open the input image and get its geotransform and projection
+#     input_ds = gdal.Open(input_path)
+#     geotransform = input_ds.GetGeoTransform()
+#     projection = input_ds.GetProjection()
+#     input_ds = None  # Close the dataset
 
-    # Normalize the output image to [0, 1]
+#     # Normalize the output image to [0, 1]
   
-    # Create the output dataset
-    driver = gdal.GetDriverByName('GTiff')
-    output_ds = driver.Create(output_path, output_image.shape[1], output_image.shape[0], 1, gdal.GDT_Float32)
+#     # Create the output dataset
+#     driver = gdal.GetDriverByName('GTiff')
+#     output_ds = driver.Create(output_path, output_image.shape[1], output_image.shape[0], 1, gdal.GDT_Float32)
 
-    # Set the geotransform and projection
-    output_ds.SetGeoTransform(geotransform)
-    output_ds.SetProjection(projection)
+#     # Set the geotransform and projection
+#     output_ds.SetGeoTransform(geotransform)
+#     output_ds.SetProjection(projection)
 
-    # Write the output image
-    output_ds.GetRasterBand(1).WriteArray(output_image)
-    output_ds = None  # Close the dataset
+#     # Write the output image
+#     output_ds.GetRasterBand(1).WriteArray(output_image)
+#     output_ds = None  # Close the dataset
     
 def preprocess_image_with_exr(input_path, output_path):
     # Load the input image
@@ -73,33 +72,41 @@ def preprocess_image_with_exr(input_path, output_path):
     binary_mask = binary_mask / 255.0
 
     # Save the binary mask with the geoinformation of the input image
-    save_image_with_geoinfo(input_path, binary_mask, output_path)
+    # save_image_with_geoinfo(input_path, binary_mask, output_path)
+    
+    return binary_mask
 
 import os
 import numpy as np
 
-def remove_images_with_low_green_pixels(image_folder, mask_folder, threshold=0.9):
+def remove_images_with_low_green_pixels(image_folder, mask_folder, threshold=0.72):
     # Get the list of image files
     image_files = os.listdir(image_folder)
 
     for image_file in image_files:
-        # Construct the full path of the image file and the corresponding mask file
-        image_path = os.path.join(image_folder, image_file)
-        mask_path = os.path.join(mask_folder, image_file)
+       try:
+            
+            # Construct the full path of the image file and the corresponding mask file
+            image_path = os.path.join(image_folder, image_file)
+            mask_path = os.path.join(mask_folder, image_file)
 
-        # Preprocess the image and save the result
-        preprocess_image_with_exr(image_path, 'temp.tif')
+            # Preprocess the image and save the result
+            preprocessed_image = preprocess_image_with_exr(image_path, 'temp.tif')
 
-        # Load the preprocessed image
-        preprocessed_image = cv2.imread('temp.tif', cv2.IMREAD_GRAYSCALE)
+            # Load the preprocessed image
+            #preprocessed_image = cv2.imread('temp.tif', cv2.IMREAD_GRAYSCALE)
 
-        # Calculate the ratio of green pixels
-        green_ratio = np.count_nonzero(preprocessed_image) / (preprocessed_image.shape[0] * preprocessed_image.shape[1])
-
-        # If the ratio of green pixels is below the threshold, remove the image and the mask
-        if green_ratio < threshold:
-            os.remove(image_path)
-            os.remove(mask_path)
+            # Calculate the ratio of green pixels
+            zero_ratio = np.count_nonzero(preprocessed_image) / (preprocessed_image.shape[0] * preprocessed_image.shape[1])
+            green_ratio = np.count_nonzero(np.logical_not(preprocessed_image)) / (preprocessed_image.shape[0] * preprocessed_image.shape[1])
+            
+            # If the ratio of green pixels is below the threshold, remove the image and the mask
+            if green_ratio < threshold:
+                os.remove(image_path)
+                os.remove(mask_path)
+                print(f"{image_file} 1픽셀 비율 : {green_ratio} / 0픽셀 비율 : {zero_ratio}")
+       except:
+           print(f"{image_file} 읽을 수  없음")
 
 # Call the function
-remove_images_with_low_green_pixels('path_to_image_folder', 'path_to_mask_folder')
+remove_images_with_low_green_pixels('/data/cabbage_3v/Dataset/train/image', '/data/cabbage_3v/Dataset/train/mask')
